ef42a6898e992dd19f825ca4cf00c2c2df80267b
kofinartey <eugeneahulu15@live.com>
2022-07-29 23:30:45 +0000

    completed creations

--- src/components/creations/Creations.tsx
+++ src/components/creations/Creations.tsx
@@ -1,12 +1,32 @@
 import React from "react";
 import styles from "./Creations.module.css";
+import creationsData from "../../data/data.json";
 
 function Creations() {
   return (
-    <section>
-      <div className={styles.top}>
-        <h2>Our Creations</h2>
-        <button> See all</button>
+    <section className={styles.Creations}>
+      <div className={styles.wrapper}>
+        <div className={styles.top}>
+          <h2>Our Creations</h2>
+          <button> See all</button>
+        </div>
+        <div className={styles.creationsContainer}>
+          {creationsData.map((creation) => (
+            <div className={styles.creation} key={creation.text}>
+              <h3 className={styles.text}>{creation.text}</h3>
+              <picture>
+                <source
+                  srcSet={process.env.PUBLIC_URL + `${creation.images.desktop}`}
+                  media="(min-width: 48rem)"
+                ></source>
+                <img
+                  src={process.env.PUBLIC_URL + `${creation.images.mobile}`}
+                  alt=""
+                />
+              </picture>
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   );
